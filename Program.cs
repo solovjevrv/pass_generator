fc94dda1d6c52ac8bbcd1b64e0d269791bd0a052
solovjevrv <solovjevrv@gmail.com>
2022-08-08 22:47:08 +0300

    Описан метод генерации пароля

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string GeneratePassword (int LenghtPassword)
+{
+	var rand = new Random();
+
+	string password = string.Empty;
+	for (int i=0;i<LenghtPassword/4;i++)
+	{
+			password += (char)rand.Next('a', 'z');
+			password += (char)rand.Next('A', 'Z');
+			password += rand.Next(1, 10);
+			password += (char)rand.Next('!', ')');
+			
+	}
+	return password;
+}
+
+System.Console.WriteLine(GeneratePassword(8));
+
